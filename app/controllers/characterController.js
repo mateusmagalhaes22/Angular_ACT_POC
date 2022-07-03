3ab15ab84e77eef5a938bae0afa3e8987cd02bbf
Mateus Magalhães <magalhaes.mateus22@gmail.com>
2022-07-03 14:32:11 -0300

    local initial commit

---
+++ app/controllers/characterController.js
@@ -0,0 +1,7 @@
+angular.module("app").controller("CharacterController");
+function CharacterController() {
+    const characterController = this;
+    characterController.listByNameStartsWith = () => {
+        
+    }
+}

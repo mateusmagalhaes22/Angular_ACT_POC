4b261650e404a7a3d962fcf8bb1cec4fbb9486f6
Mateus Magalhães <magalhaes.mateus22@gmail.com>
2022-07-04 10:10:01 -0300

    typeError no metodo listar consertado

--- app/characterController.js
+++ app/characterController.js
@@ -1,9 +1,23 @@
-angular.module("marvelApp", []).controller("CharacterController", function() {
+angular.module("marvelApp", []).controller("CharacterController", function($q, $http) {
     const characterController = this;
     characterController.characterName = '';
     characterController.characterList = [];
-    characterController.search = () => {
+    characterController.search = function(service) {
         characterController.characterList = [];
-        characterController.characterList.push(characters.listar(this.characterName));
+        characterController.characterList.push(characterController.listar(characterController.characterName));
     };
+    characterController.listar = (nome) => {
+        var promise = $q.defer();
+        var list = [];
+        $http.get(`https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db&nameStartsWith=${nome}`).then(
+            function(result){
+                angular.forEach(result, function(){
+                    console.log('1 '+result)
+                    list.push(result);
+                });
+                promise.resolve(list);
+            }
+        );
+        return list;
+    }
 });

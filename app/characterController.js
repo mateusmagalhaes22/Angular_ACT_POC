bde1ef217e2cfbf1005e8dd13234efa3be7dc6ab
Mateus Magalhães <magalhaes.mateus22@gmail.com>
2022-07-04 10:46:21 -0300

    ultimo commit: um pouco de otimização do código

--- app/characterController.js
+++ app/characterController.js
@@ -12,7 +12,6 @@ angular.module("marvelApp", []).controller("CharacterController", function($q, $
         $http.get(`https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db&nameStartsWith=${nome}`).then(
             function(result){
                 angular.forEach(result, function(){
-                    console.log('1 '+result)
                     list.push(result);
                 });
                 promise.resolve(list);

b57ecfd8c945eb1d9d5f1f74eb2f0478e74879ff
Mateus Magalhães <magalhaes.mateus22@gmail.com>
2022-07-03 23:28:44 -0300

    erro '' consertado

--- app/characterFactory.js
+++ app/characterFactory.js
@@ -5,10 +5,10 @@ angular.module("marvelApp", []).factory("Characters", function($q, $http){
             var list = [];
             $http.get(`https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db&nameStartsWith=${nome}`).then(
                 list = function(result){
-                    angular.forEach(result, function(characters){
-                        console.log(characters)
-                        list.push(characters);
-                    })
+                    angular.forEach(result, function(){
+                        console.log(result)
+                        list.push(result);
+                    });
                     promise.resolve(list);
                 }
             );
